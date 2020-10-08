f35a76b9310970d3d82b1bc56abe0c31ecbf0ed6
Archana Kushwaha <70112830+Archana-kushwaha@users.noreply.github.com>
2020-10-08 22:14:48 +0530

    Add files via upload

---
+++ day1project.py
@@ -0,0 +1,27 @@
+Day = 3
+if Day == 1:
+   print ("MONDAY")
+   print (Day)
+elif Day == 2:
+   print ("TUESDAY")
+   print (Day)
+elif Day == 3:
+   print("WEDNESDAY")
+   print(Day)
+elif Day == 4:
+   print("THURSDAY")
+   print(Day)
+elif Day == 5:
+   print ("FRIDAY")
+   print (Day)
+elif Day == 6:
+   print ("SATURDAY")
+   print (Day)
+elif Day == 7:
+   print("SUNDAY")
+   print(Day)
+else:
+   print ("INVALID DAY")
+   print (Day)
+
+print ("Good bye!")
