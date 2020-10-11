b0dafb0eb5bdd88cf90f691b776db7147d025f3f
Archana Kushwaha <70112830+Archana-kushwaha@users.noreply.github.com>
2020-10-11 14:27:46 +0530

    Update day1project.py

--- day1project.py
+++ day1project.py
@@ -1,27 +1,27 @@
-Day = 3
-if Day == 1:
+input_day = 3
+if input_day == 1:
    print ("MONDAY")
-   print (Day)
-elif Day == 2:
+   print (input_day)
+elif input_day == 2:
    print ("TUESDAY")
-   print (Day)
-elif Day == 3:
+   print (input_day)
+elif input_day== 3:
    print("WEDNESDAY")
-   print(Day)
-elif Day == 4:
+   print(input_day)
+elif input_day == 4:
    print("THURSDAY")
-   print(Day)
-elif Day == 5:
+   print(input_day)
+elif input_day == 5:
    print ("FRIDAY")
-   print (Day)
-elif Day == 6:
+   print (input_day)
+elif input_day == 6:
    print ("SATURDAY")
-   print (Day)
-elif Day == 7:
+   print (input_day)
+elif input_dayy == 7:
    print("SUNDAY")
-   print(Day)
+   print(input_day)
 else:
    print ("INVALID DAY")
-   print (Day)
+   print (input_day)
 
 print ("Good bye!")
