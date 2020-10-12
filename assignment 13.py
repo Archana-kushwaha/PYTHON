fd12bb0fb8eab2a23f8201fc9b075f0546eefdcd
Archana Kushwaha <70112830+Archana-kushwaha@users.noreply.github.com>
2020-10-12 16:51:06 +0530

    Add files via upload

---
+++ assignment 13.py
@@ -0,0 +1,5 @@
+list_numbers = [18, 30, 14, 20, 9, 25, 4, 1]
+
+list_numbers.sort(reverse=True)
+
+print(list_numbers) 
---
+++ assignment 8.py
@@ -0,0 +1,6 @@
+list1 =[12, 45, 2, 41, 31, 10, 8, 6, 4]
+
+list1.sort()
+
+print("Second largest element is:", list1[-2])
+print("Second smallest element is:", list1[+1])
